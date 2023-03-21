b0466ab5f6fb99f114bc3b5e3b8c178e98abb785
RafaelMello31 <102549786+RafaelMello31@users.noreply.github.com>
2023-03-20 22:41:42 -0300

    Create DListNode.java

---
+++ DListNode.java
@@ -0,0 +1,22 @@
+package uscs;
+
+public class DListNode {
+	
+public int item;
+public DListNode next;
+public DListNode prev;
+
+public DListNode() {
+	this.item = 0;
+	this.next = null;
+	this.prev = null;
+
+}
+
+public DListNode (int item) {
+	this.item = item;
+	this.next = null;
+	
+}
+
+}
